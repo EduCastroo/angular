7dc02bacfe570e537d1504cadf785452defc87f4
EduCastroo <educastro._@hotmail.com>
2021-12-02 11:37:14 -0300

    add rotas

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "", redirectTo: "entrar", pathMatch: "full"},
+{path: "cadastrar", component: CadastrarComponent},
+{path: "entrar", component: EntrarComponent},
+{path: "menu", component: MenuComponent},
+{path: "rodape", component: RodapeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
